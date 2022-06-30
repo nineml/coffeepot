aca0e7587d9072eb41cf65dcc9242ac8fca3ab91
Norman Walsh <ndw@nwalsh.com>
2022-06-30 18:44:42 +0100

    Fix JavaDoc errors

--- src/main/java/org/nineml/coffeepot/utils/ParserOptions.java
+++ src/main/java/org/nineml/coffeepot/utils/ParserOptions.java
@@ -33,6 +33,7 @@ public class ParserOptions extends org.nineml.coffeefilter.ParserOptions {
      * The cache directory.
      * <p>If this is not null, it should be the name of a directory on the
      * local filesystem that can be used as a cache location.</p>
+     * @return the cache directory
      */
     public String getCacheDir() {
         return cacheDir;
@@ -55,6 +56,7 @@ public class ParserOptions extends org.nineml.coffeefilter.ParserOptions {
      * {@link System#out} is redirected. If the value is "tty", no progress messages
      * will be written to the output in this case. If the value is "true", the progress
      * messages will be written to the redirected location.</p>
+     * @return the progress bar display setting.
      */
     public String getProgressBar() {
         return progressBar;
@@ -79,6 +81,7 @@ public class ParserOptions extends org.nineml.coffeefilter.ParserOptions {
      * <p>The first character is used for an "empty" space in the progress bar.
      * The last character is used for a "full" space. Any characters in between
      * the first and the last are used for even fractions between 0 and 1.</p>
+     * @return the characters used to create the status bar.
      */
     public String getProgressBarCharacters() {
         return barCharacters;
@@ -104,6 +107,7 @@ public class ParserOptions extends org.nineml.coffeefilter.ParserOptions {
      * Make sure the output ends with a newline?
      * <p>If this option is true, a newline will be added to the end of the output.
      * </p>
+     * @return the trailing newline setting.
      */
     public boolean getTrailingNewlineOnOutput() {
         return trailingNewlineOnOutput;
